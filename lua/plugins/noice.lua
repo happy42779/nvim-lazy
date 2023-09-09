6d74c2de2ec8fdac1a56ec99fe777a031b17a6ea
Ian <emailforgong@gmail.com>
2023-09-09 16:49:43 +0800

    centered the cmdline ui

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -14,12 +14,14 @@ return {
 			override = {
 				["vim.lsp.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_document"] = true,
 			}
 		},
 		presets = {
 			bottom_search = true,
-			command_palette = true,
+			command_palette = false,
 			long_message_to_split = true,
+			-- inc_rename = true
 		}
 	},
 }
