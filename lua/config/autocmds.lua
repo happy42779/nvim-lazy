fe5ef57ce2996e78d7e269d65e159706315885e5
Ian <emailforgong@gmail.com>
2023-09-15 22:32:19 +0800

    added autocmds to unlink jump locations of snips

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,6 +14,17 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
+-- solve the tab jumping to unfinished snip locations
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "*",
+	callback = function()
+		if ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i") and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()] and not require("luasnip").session.jump_active
+		then
+			require("luasnip").unlink_current()
+		end
+	end
+})
+
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup("close_with_q"),
